40af17b0482a26563d7a2a18f76f673a9b167351
linos nchena <linosnchena@gmail.com>
2019-12-21 16:45:19 +0100

    consolidation from two to one module

--- marvin/src/main/java/bankfocus/Smallestx1.java
+++ marvin/src/main/java/bankfocus/Smallestx1.java
@@ -3,11 +3,12 @@ public class Smallestx1
 {
 	public static void main(String[] args) 
 	{
-	 int[] a = {1984, 2019, 2009, 2004, 2020};
+	 int[] a = {1984, 2019, 2009, 2004, 33, 66,2020,4};
+	 int d=a.length;
 		int temp;  
 		
-     for (int i = 0; i < a.length; i++) 		 {  
-		 for (int j = i + 1; j < a.length; j++)   
+     for (int i = 0; i < d; i++) 		 {  
+		 for (int j = i + 1; j < d; j++)   
 		 {  
 			 if (a[i] < a[j])   
 			 { 	 temp = a[i];  
@@ -15,10 +16,13 @@ public class Smallestx1
 				 a[j] = temp;   }  
 			 }  
 		 }  
-	 System.out.print("\nSecondlargest number is:" + a[1]);
+	System.out.println("============================ STARTx1 =====================");
+	System.out.print("\nSecondlargest number is:" + a[1]+"\n");
 	  // 2nd element because index starts from 0 
-	 System.out.print("\nFirst largest number is:" + a[0]);
+	System.out.print("\nFirst largest number is:" + a[0]+"\n");
 	 //  1st element because index starts from 0  
+	 System.out.print("\nThe smallest number is:" + a[d-1]+"\n");
+	System.out.println("=============================== ENDx2 =====================");
 	
 	} 
 }
--- marvin/src/main/java/bankfocus/Smallestx2.java
+++ marvin/src/main/java/bankfocus/Smallestx2.java
@@ -1,30 +1,34 @@
 package bankfocus;
-public class Smallestx2{  
+public class Smallestx2 {  
 	public static void main(String[] args) {
-		int arr[] = { 1812, 1984, 2009, 2014 };
+		int arr[] = { 8,809, 12, 194, 209, 214, 66,3,424 };
+		int d=arr.length;
 		int largest = arr[0];
 		int secondLargest = arr[0];		
-		System.out.println("The given array is:" );
+		System.out.println("The last six humidity readings are:" );
 		for (int i = 0; i < arr.length; i++) 
-		{	System.out.print(arr[i]+"\t");	System.out.print(arr[i]+"\n");	}	
-		System.out.println("================================ ENDX1 =================");
-		for (int i = 0; i < arr.length; i++) {
- 			if (arr[i] > largest) {
-				secondLargest = largest;  //Initial guess move to second place
+		{	System.out.print(arr[i]+"\t");	
+			System.out.print(arr[i]+"\n");
+		}	
+		System.out.println("============================= ENDx1 =================");
+		for (int i = 0; i < d; i++) {
+ 			if (arr[i] > largest && arr[i] > secondLargest) {
+				secondLargest = largest;  // yes, the largest is demoted second
 				largest = arr[i];          // trial becomes number one
- 			} else if (arr[i] > secondLargest) { // Second competition
-				secondLargest = arr[i];     //guess is further demoted to third!
+			 }
+			 else if (arr[i] > secondLargest) { // Second competition
+				secondLargest = arr[i];     //yes is further demoted to third!
  			}
 		}
-		System.out.println("============================= STARTX2 ====================");
-        System.out.print("\nFirst largest number is:" + largest);
-		System.out.println("\nSecond largest number is:" + secondLargest);
-		System.out.println("=========================== MID ======================");
-		System.out.print("\nSecondlargest number is:" + arr[1]);
+	
+        System.out.print("\nFirst largest unit is:" + largest);
+		System.out.println("\nSecond largest unit is:" + secondLargest);
+		System.out.println("============================= MID ======================");
+			System.out.print("\nFirst measurment is:" + arr[0]);
 	    	// 2nd element because index starts from 0 
-	           System.out.print("\nFirst largest number is:" + arr[0]+"\n");
+	        System.out.print("\nLast measurment is:" + arr[d-1]+"\n");
 	      //  1st element because index starts from 0  
-	     System.out.println("============================ END =====================");
+	     System.out.println("============================ ENDx2 =====================");
  	}
 }
 //	1-2-3
