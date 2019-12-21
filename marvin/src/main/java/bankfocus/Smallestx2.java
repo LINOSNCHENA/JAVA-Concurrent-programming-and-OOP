67789e9a6022624142e2c5428ed6e3efdca001d8
linos nchena <linosnchena@gmail.com>
2019-12-21 16:49:18 +0100

    consolidation from two to one module

--- marvin/src/main/java/bankfocus/Smallestx2.java
+++ marvin/src/main/java/bankfocus/Smallestx2.java
@@ -4,13 +4,14 @@ public class Smallestx2 {
 		int arr[] = { 8,809, 12, 194, 209, 214, 66,3,424 };
 		int d=arr.length;
 		int largest = arr[0];
-		int secondLargest = arr[0];		
+		int secondLargest = arr[0];	
+		System.out.println("============================= START =================");	
 		System.out.println("The last six humidity readings are:" );
 		for (int i = 0; i < arr.length; i++) 
 		{	System.out.print(arr[i]+"\t");	
 			System.out.print(arr[i]+"\n");
 		}	
-		System.out.println("============================= ENDx1 =================");
+		// System.out.println("============================= START =================");
 		for (int i = 0; i < d; i++) {
  			if (arr[i] > largest && arr[i] > secondLargest) {
 				secondLargest = largest;  // yes, the largest is demoted second
@@ -23,12 +24,12 @@ public class Smallestx2 {
 	
         System.out.print("\nFirst largest unit is:" + largest);
 		System.out.println("\nSecond largest unit is:" + secondLargest);
-		System.out.println("============================= MID ======================");
+		System.out.println("========================= SUMMARY =====================");
 			System.out.print("\nFirst measurment is:" + arr[0]);
 	    	// 2nd element because index starts from 0 
 	        System.out.print("\nLast measurment is:" + arr[d-1]+"\n");
 	      //  1st element because index starts from 0  
-	     System.out.println("============================ ENDx2 =====================");
+	     System.out.println("========================= END =======================");
  	}
 }
 //	1-2-3
