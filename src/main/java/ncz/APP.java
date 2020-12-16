e9cccc0c127bda39dd76102e76e4e015a59c5fe4
Linos Nchena <linosnchena@gmail.com>
2020-12-16 18:36:34 +0100

    added extrems

--- src/main/java/ncz/APP.java
+++ src/main/java/ncz/APP.java
@@ -5,21 +5,25 @@ public class APP {
   public static void main(String[] args) {
 
     // INPUTS AND CONDITIONS TESTED (FIRST-RANKING)
-    // int A[] = { 3, 4, 5, 6, 7, 8, 9, 10,11,12 }; // Test #1 SORT +MAX-10
-     int A[] = {10, 5, 2, 3, 4, 5, 6, 8,7,9 }; // Test #2 RANGE ELEMENTS
-    // int A[] = { -4,-3,-2,-1,0 }; // Test #3 ZEROS AS LAST ELEMENT
-    // int A[] = { 1, 2, 4, 6,5,6, 7, 8, 9 }; // Test #3 BAD-GAP NOT ONE/ORDER
-    // int A[] = {-3,-2,-1}; // Test #5 NEGATIVES AS THE LAST 1 == 1
-   // int A[] = {1, 3, 6, 4, 1, 2 }; // Sorting 5
+   //  int A[] = { 3, 4, 5, 6, 7, 8, 9, 10,11,12 }; // Test #1 SORT +MAX-10
+    //int A[] = {10, 5, 2, 3, 4, 5, 6, 8,7,9 }; // Test #2 RANGE ELEMENTS
+   //  int A[] = { -4,-3,-2,-1,0 }; // Test #3 ZEROS AS LAST ELEMENT
+  // int A[] = { 1, 2, 4, 6,5,6, 7, 8, 9 }; // Test #3 BAD-GAP NOT ONE/ORDER
+ //    int A[] = {-3,-2,-1}; // Test #5 NEGATIVES AS THE LAST 1 == 1
+    //int A[] = {1, 3, 6, 4, 1, 2 }; // Sorting 5
+    int A[] = {-10013, -100012, -100011, }; // Sorting 5
+   // int A[] = {88871}; // Sorting 5
 
-    ArrayD job2 = new ArrayD();
-    int Y = job2.sortAddArray(A);
-    System.out.print(Y);
-    System.out.println("\n ================== FIRST PRODUCED =================" + "\n");
-    ArrayA job = new ArrayA();
-    int z = job.sortAddArray(A);
+
+    // System.out.println("\n ================== FIRST-A PRODUCED =================" + "\n");
+    // ArrayD job2 = new ArrayD();
+    // int Y = job2.sortAddArray(A);
+    // System.out.print(Y);
+    System.out.println("\n ================== FIRST-D PRODUCED =================" + "\n");
+    ArrayE job = new ArrayE();
+    int z = job.solution(A);
     System.out.print(z);
-    System.out.println("\n ================== SECOND PRODUCED =================" + "\n");
+    System.out.println("\n ================== THIRD-E PRODUCED =================" + "\n");
 
   }
 }
--- src/main/java/ncz/ArrayD.java
+++ src/main/java/ncz/ArrayD.java
@@ -5,18 +5,27 @@ public class ArrayD {
     int sortAddArray(int[] A) {
         int sizeX = A.length;
         int valueX = 99;
-
+  
         Arrays.sort(A);
         for (int s = 0; s < sizeX - 1; s++) {
+            System.out.println(sizeX);
+            System.out.print("sizeX");
+
+            // if ((sizeX) ==1) {
+            //     valueX = 1 + A[s];
+            //     return valueX;//7
+
+            // } 
             if (sizeX > 22) {
                 valueX = -1;
                 return valueX;//1
             }
-            if (A[s] < -14 || A[s] > 14 || A[s + 1] < -15 || A[1 + s] > 15) {
+            if (A[s] < -1000000 || A[s] > 1000000) {
+                valueX = -2;
                 return valueX;//2
             }
             if ((A[s + 1] - A[s]) > 2) {
-                valueX =-2;
+                valueX =-3;
                 return valueX;//3
 
             } 
@@ -35,12 +44,21 @@ public class ArrayD {
                 return valueX;//6
 
             }
+            if ((A[s + 1] - A[s]) < 0) {
+                valueX =-2;
+                System.out.print("Test7");
+                return valueX;//7
+
+            } 
+       
      
             
             else {
-                valueX = 1 + A[sizeX - 1];//7
+                System.out.print("Test8");
+                valueX = 1 + A[sizeX - 1];//8
             }
         }
+        System.out.println("Test8");
         return valueX;
     }
 }
