4c24d0a45530c95e586d8f5450dd5edfef0a977d
Linos Nchena <linosnchena@gmail.com>
2020-12-17 04:36:48 +0100

    55 percentage

--- src/main/java/ncz/APP.java
+++ src/main/java/ncz/APP.java
@@ -8,16 +8,16 @@ public class APP {
     // int A[] = { 3, 4, 5, 6, 7, 8, 9, 10,11,12 }; // Test #1 SORT +MAX-10
     // int A[] = { 10, 5, 9, 9, 9, 2, 3, 1, 5, 6, 8, 7, 9 }; // RANGEELEMENTS
   //   int A[] = { -4,-3,-2,-1,0 }; // Test #3 ZEROS AS LAST ELEMENT
-    int A[] = { 1, 2, 4, 6,5,6, 7, 8, 9 }; // Test #3 BAD-GAP NOT ONE/ORDER
+    //int A[] = { 1, 2, 4, 6,5,6, 7, 8, 9 }; // Test #3 BAD-GAP NOT ONE/ORDER
     // int A[] = {-3,-2,-1}; // Test #5 NEGATIVES AS THE LAST 1 == 1
-    // int A[] = {-33,-32,-31}; // Test #5 NEGATIVES AS THE LAST 1 == 1
+     int A[] = {-33,-32,-31}; // Test #5 NEGATIVES AS THE LAST 1 == 1
     // int A[] = {1, 3, 6, 4, 1, 2 }; // Sorting 5
-   //  int A[] = {1};//-10013, -100012, -100011, }; // Sorting 5
+  //  int A[] = {1};//-10013, -100012, -100011, }; // Sorting 2
    // int A[] = { 1, 2, 3 }; // Sorting 1
     // int A[]={4, 5, 6, 2}; // BACK TO ONE1
     // int A[]={1, 3, 6, 4, 1, 2};// [1, 3, 6, 4, 1, 2] //5
     System.out.println("\n ================== FINAL-NCHENA-PRODUCED =================" + "\n");
-    ArrayN job3 = new ArrayN();
+    ArrayO job3 = new ArrayO();
     int Y1 = job3.solution(A);
     System.out.print(Y1);
   }
---
+++ src/main/java/ncz/ArrayO.java
@@ -0,0 +1,52 @@
+package ncz;
+
+import java.util.Arrays;
+
+public class ArrayO {
+    static int removeDuplicates(int arr[], int n) {
+        if (n == 0 || n == 1)
+            return n;
+        int[] temp = new int[n];
+        int j = 0;
+        for (int i = 0; i < n - 1; i++)
+            if (arr[i] != arr[i + 1])
+                temp[j++] = arr[i];
+        temp[j++] = arr[n - 1];
+        for (int i = 0; i < j; i++)
+            arr[i] = temp[i];
+        return j;
+    }
+
+    public int solution(int[] A) {
+        int n = A.length;
+        Arrays.sort(A);
+
+        Arrays.sort(A);
+        for (int i = 0; i < n; i++) {
+            if ((n) == 1) {
+                if (A[i] < -1000000) {
+                    return 1;
+                }
+                if (A[i] > 1000000) {
+                    return 1;
+                }
+                if (A[i] == 1) {
+                    return 2;
+                }
+                n = 1;
+                return n;
+            }
+        }
+
+        n = removeDuplicates(A, n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(A[i] + "=|=" + (i + 1)); // MONZE
+            if (A[i] != (i + 1)) {
+                n = (i + 1);
+                return n;
+            }
+        }
+        return n + 1;
+    }
+}
